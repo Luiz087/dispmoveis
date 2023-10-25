2df29150f23d9cfed557bbd8af25a1b5df120297
Luiz Fernando <luiz.s08@aluno.ifsc.edu.br>
2023-10-25 11:33:53 -0300

    Inserção de Nota Lista

---
+++ SQLDataBase/app/src/main/java/com/example/sqldatabase/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.sqldatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    SQLiteDatabase db;
+    Button button;
+
+    EditText editText;
+    ListView listView;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        db = openOrCreateDatabase("meubd",MODE_PRIVATE,null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo VARCHAR, txt var)");
+        button = findViewById(R.id.botao);
+        listView = findViewById(R.id.lista);
+        editText = findViewById(R.id.editText);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insereNota();
+            }
+        });
+    }
+
+    public void insereNota(){
+        ContentValues cv = new ContentValues();
+        cv.put("titulo", String.valueOf(editText.getText()));
+        db.insert("notas", null, cv);
+        loadListView();
+    }
+
+    public void loadListView(){
+        Cursor cursor = db.rawQuery("SELECT * FROM notas", null);
+        cursor.moveToFirst();
+        ArrayList<String> listaNotas = new ArrayList<>();
+        while(!cursor.isAfterLast()){
+            listaNotas.add(cursor.getString(cursor.getColumnIndex("titulo")));
+            cursor.moveToNext();
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1,listaNotas);
+        listView.setAdapter(arrayAdapter);
+    }
+}
